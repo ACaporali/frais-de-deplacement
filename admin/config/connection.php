5a8a68f00e291068cb71b2fe683d5a1956a5ba32
Alice Caorali <alice.caporali@hotmail.fr>
2018-10-06 21:07:43 +0200

    Add heroku config vars for the database

--- admin/config/connection.php
+++ admin/config/connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $bdd = new PDO('mysql:host=localhost; dbname=api', 'root', '');
+  $bdd = new PDO('mysql:host='.getenv('db_host').'; dbname='.getenv('db_name').'', getenv('db_user'), getenv('db_pwd'));
 }
 catch(exception $e) {
   die('Erreur '.$e->getMessage());
