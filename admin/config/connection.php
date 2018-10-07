71f01b285ffc232972bf7482ace578a108c03197
Alice Caorali <alice.caporali@hotmail.fr>
2018-10-07 11:11:17 +0200

    Show host db if connection fail

--- admin/config/connection.php
+++ admin/config/connection.php
@@ -3,6 +3,6 @@ try {
   $bdd = new PDO('mysql:host='.getenv('db_host').'; dbname='.getenv('db_name').'', getenv('db_user'), getenv('db_pwd'));
 }
 catch(exception $e) {
-  die('Erreur '.$e->getMessage());
+  die('Erreur '.$e->getMessage().' | '.getenv('db_host'));
 }
 ?>
