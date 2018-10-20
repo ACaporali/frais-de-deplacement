c460626d93fe8757da59c2cf2f79ba4d40242c92
Alice Caorali <alice.caporali@hotmail.fr>
2018-10-20 21:19:38 +0200

    Add echo log

--- excel/index.php
+++ excel/index.php
@@ -8,6 +8,7 @@
   require_once('src/app/Controllers/FileController.php');
 
   if (isset($_POST['submit'])) {//src/app/utils/upload.php
+    echo "isset($_POST['submit']";
     $file = new FileController();
     $isUploaded = $file->upload($_FILES["fileToUpload"]);
 
@@ -18,6 +19,7 @@
       $isEdited = $app->editeExcelFile($isUploaded['file']->getName());
     }
   } else {
+    echo "Form-upload-file.php";
     require_once('src/app/Views/Form-upload-file.php');
   }
 
