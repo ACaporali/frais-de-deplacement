0f3ba9b140ba0f27e1398c4aff9a500158fd39e6
Alice Caorali <alice.caporali@hotmail.fr>
2018-10-28 09:44:32 +0100

    Remove echo in index

--- excel/index.php
+++ excel/index.php
@@ -8,8 +8,7 @@
   require_once('src/app/Controllers/FileController.php');
 
 echo "beford if";
-  if (isset($_POST['submit'])) {//src/app/utils/upload.php
-    echo "POST['submit']";
+  if (isset($_POST['submit'])) {
     $file = new FileController();
     $isUploaded = $file->upload($_FILES["fileToUpload"]);
 
@@ -20,10 +19,7 @@ echo "beford if";
       $isEdited = $app->editeExcelFile($isUploaded['file']->getName());
     }
   } else {
-    echo "Form-upload-file.php";
     require_once('src/app/Views/Form-upload-file.php');
   }
-
-  echo "after if";
 
 ?>
