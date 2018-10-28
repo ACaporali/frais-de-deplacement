e98b2a8e656148a35640e8aa10a9db51d68483d8
Alice Caorali <alice.caporali@hotmail.fr>
2018-10-28 09:12:53 +0100

    Add msg in view add city no maj

--- admin/app/Views/city/create.php
+++ admin/app/Views/city/create.php
@@ -1,5 +1,6 @@
 <h1>Creation d'une nouvelle ville</h1>
 <a href="./">Retour accueil</a>
+<p>/!\ Ne pas mettre de majuscule dans le nom de la ville /!\</p>
 <form method="POST" action="index.php?action=createCity">
   <label for="name">Nom : </label><input type="text" name="name" placeholder="Nom de la nouvelle ville">
   <input type="submit" value="Ajouter">
